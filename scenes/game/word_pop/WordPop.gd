81fd3393cdac79e41486e446351e2cc350ef8123
ShmyQ <wschymik@gmail.com>
2024-01-12 23:10:14 -0500

    game styling

--- scenes/game/word_pop/WordPop.gd
+++ scenes/game/word_pop/WordPop.gd
@@ -4,4 +4,6 @@ func _ready():
 	var tween = create_tween()
 	tween.tween_property(self, "position", position + Vector2(0, -50), 2)
 	tween.set_trans(Tween.TRANS_EXPO).parallel().tween_property($Word, "theme_override_colors/font_color", Color(0, 0, 0, 0), 2)
+	tween.set_trans(Tween.TRANS_EXPO).parallel().tween_property($Word, "theme_override_colors/font_shadow_color", Color(0, 0, 0, 0), 2)
+	tween.set_trans(Tween.TRANS_EXPO).parallel().tween_property($Word, "theme_override_colors/font_outline_color", Color(0, 0, 0, 0), 2)
 	tween.tween_callback(func(): queue_free())
