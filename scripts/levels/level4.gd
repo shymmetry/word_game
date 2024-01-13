189d4c21649155e3654e2efb480db47b2865264a
ShmyQ <wschymik@gmail.com>
2024-01-12 19:33:12 -0500

    small changes

--- scripts/levels/level4.gd
+++ scripts/levels/level4.gd
@@ -1,8 +1,8 @@
 extends Node
 
 const win_type = E.WIN_TYPES.WORD_SIZE
-const win_threshold = 1
-const win_text = "Score 1 Word With 6+ Letters"
+const win_threshold = 2
+const win_text = "Score 2 Word With 6+ Letters"
 const win_data = {
 	"word_size": 6
 }
