0fdfc98ab4948d1e68351d576bb63134f8d39b37
ShmyQ <wschymik@gmail.com>
2024-01-15 16:20:08 -0500

    change level order handling

--- scripts/levels.gd
+++ scripts/levels.gd
@@ -2,14 +2,21 @@ extends Node
 
 const level_defaults = preload("res://scripts/levels/level_defaults.gd")
 
+# Ordered list of levels as their file names
+const level_order = [
+	"endless",
+	"basic",
+	"basic2",
+	"word_size",
+	"word_size2",
+	"wildcard_intro",
+	"impossible",
+]
+
 func set_current_level(level: int):
 	Globals.current_level = level
 	
-	var level_data
-	if level == 0:
-		level_data = load("res://scripts/levels/endless.gd")
-	else:
-		level_data = load("res://scripts/levels/level%s.gd" % level)
+	var level_data = load("res://scripts/levels/%s.gd" % level_order[level])
 	
 	var config_map = level_defaults.new().get_config_map()
 	for key in config_map:
